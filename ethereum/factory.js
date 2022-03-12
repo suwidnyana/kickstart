a6bc96b971213f226bec0d63082388eaea7d912e
suwidnyana <grayfullbuster522@gmail.com>
2022-03-12 20:17:57 +0800

    finish boilerplate

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,8 +2,8 @@ import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
-  compiledFactory.abi,
-  '0xdc89d37e1f76f3c9e8466c595f301c62763227a0'
+  CampaignFactory.abi,
+  '0x1547dCcaDFbC675e7df9c209ac868b26f174a07D'
 );
 
 export default instance;
